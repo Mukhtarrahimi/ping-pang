fd9a90e114e650fc55bd59e7e0cb2c70ed44b222
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-04 14:43:53 +0430

    all color code.

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ pygame.init()
 WIDHT, HEIGHT = 600, 500
 BALL_SPEED = 7
 PADDLE_SPEDD = 7
+
+# color
+BACKGROUND_COLOR = (15, 23, 42)     
+BALL_COLOR = (0, 255, 255)          
+PADDLE_COLOR = (168, 85, 247)      
+TEXT_COLOR = (241, 245, 249)        
+MIDLINE_COLOR = (100, 116, 139)    
+
